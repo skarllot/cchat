12e9d6d748e0e4ba88af2a8f55e16859b9fc82fc
Fabrício Godoy <skarllot@gmail.com>
2010-10-24 16:20:41 -0200

    Created string_split to split string into tokens
    > src/common/string.[hc]
    Created internal string_create_nc to allow creating string from char*
    without copying.
    Created pchar_substring to allow get substring direct from char*.
    Created type string_ll_t, a linked list to store string tokens.
    > tests/string/main.c
    Improved show_string for better readability.
    Created tests for pchar_substring and string_split.

--- tests/string/main.c
+++ tests/string/main.c
@@ -23,7 +23,7 @@
 #include "common/cstring.h"
 
 // Shows up string.
-void show_string(string_t *str);
+void show_string(const char *name, string_t *str);
 
 int main(void)
 {
@@ -33,33 +33,36 @@ int main(void)
     char str4[] = "€ÁÉÍÓÚÜ";
     char str5[] = "Á€ÉÍÓÚÜ";
     char str6[] = "Á𤭢ÉÍÓÚÜ";
+    char str7[] = "/home/user;/usr/local:/usr/share/icons;;/sbin/";
 
     string_t *tmpstr1, *tmpstr2;
+    char *tmps1;
+    string_ll_t *strll1, *strll2, *strll3, *strll4;
 
     // =========================================================================
     tmpstr1 = string_create(str1);
-    show_string(tmpstr1);
+    show_string("1 - create", tmpstr1);     // 52 chars
 
     // =========================================================================
     tmpstr2 = string_concat(tmpstr1, string_create(str2));  // <-- mem leak
-    show_string(tmpstr2);
+    show_string("2 - concat", tmpstr2);     // 62 chars
 
     string_free(tmpstr1);
 
     // =========================================================================
     tmpstr1 = string_substring(tmpstr2, 26, 26);
-    show_string(tmpstr1);
+    show_string("3 - substring", tmpstr1);      // 26 chars
 
     string_free(tmpstr2);
     string_free(tmpstr1);
 
     // =========================================================================
     tmpstr1 = string_create(str3);
-    show_string(tmpstr1);
+    show_string("4 - create (utf-8)", tmpstr1);     // 22 chars
 
     // =========================================================================
     tmpstr2 = string_substring(tmpstr1, 6, 3);
-    show_string(tmpstr2);
+    show_string("5 - substring (utf-8)", tmpstr2);  // 3 chars
 
     string_free(tmpstr2);
     string_free(tmpstr1);
@@ -67,7 +70,7 @@ int main(void)
     // =========================================================================
     tmpstr1 = string_create(str4);
     tmpstr2 = string_substring(tmpstr1, 0, 3);
-    show_string(tmpstr2);
+    show_string("6 - substring (utf-8)", tmpstr2);  // 3 chars
 
     string_free(tmpstr2);
     string_free(tmpstr1);
@@ -75,7 +78,7 @@ int main(void)
     // =========================================================================
     tmpstr1 = string_create(str5);
     tmpstr2 = string_substring(tmpstr1, 0, 3);
-    show_string(tmpstr2);
+    show_string("7 - substring (utf-8)", tmpstr2);  // 3 chars
 
     string_free(tmpstr2);
     string_free(tmpstr1);
@@ -83,19 +86,67 @@ int main(void)
     // =========================================================================
     tmpstr1 = string_create(str6);
     tmpstr2 = string_substring(tmpstr1, 0, 4);
-    show_string(tmpstr2);
+    show_string("8 - substring (utf-8)", tmpstr2);  // 4 chars
 
     string_free(tmpstr2);
     string_free(tmpstr1);
 
+    // =========================================================================
+    tmps1 = pchar_substring(str1, 5, 3);  // FGH
+    tmpstr1 = string_create(tmps1);
+    show_string("9 - substring (pchar)", tmpstr1);  // 3 chars
+
+    string_free(tmpstr1);
+    free(tmps1);
+
+    // =========================================================================
+    tmpstr1 = string_create(str3);
+    strll1 = string_split(tmpstr1, " ");
+    strll2 = strll1;
+
+    while (strll2) {
+        show_string("10 - split (utf-8)", strll2->node);
+        strll2 = strll2->next;
+    }
+
+    string_free(tmpstr1);
+    string_ll_free(strll1);
+
+    // =========================================================================
+    tmpstr1 = string_create(str7);
+    strll1 = string_split(tmpstr1, ":;");
+    strll3 = strll1;
+
+    printf("Test: 11 - split\n");
+    printf("Text: %s\n", string_get(tmpstr1));
+
+    while (strll3) {
+        printf("\t%s\n", string_get(strll3->node));
+
+        strll2 = string_split(strll3->node, "/");
+        strll4 = strll2;
+
+        while (strll4) {
+            printf("\t\t%s\n", string_get(strll4->node));
+            strll4 = strll4->next;
+        }
+
+        string_ll_free(strll2);
+        strll3 = strll3->next;
+    }
+
+    string_free(tmpstr1);
+    string_ll_free(strll1);
+    printf("\n");
+
     return 0;
 }
 
-void show_string(string_t *str)
+void show_string(const char *name, string_t *str)
 {
-    printf("=========================\n");
+    printf("Test: %s\n", name);
     printf("Length: %d\n", string_length(str));
-    printf("Value: %s\n", string_get(str));
-    printf("=========================\n");
+    printf("char length: %d\n", strlen(string_get(str)));
+    printf("Value: %s\n\n", string_get(str));
 }
 
